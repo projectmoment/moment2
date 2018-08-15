77e5223d8b490ce30e4ad33d26eaf9d7a3e73e12
Max ShinWoo Chung <donaskwhy@nate.com>
2018-08-15 22:21:16 +0000

    0816

--- app/models/board.rb
+++ app/models/board.rb
@@ -12,6 +12,6 @@ class Board < ActiveRecord::Base
     belongs_to :profile
     has_many :likes
     has_many :liked_users, through: :likes, source: :user
-    has_many :commentsz
-
+    has_many :comments
+    
 end
