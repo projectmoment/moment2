f08e8416599b41870992608ec81a9bb4c9156075
Max ShinWoo Chung <donaskwhy@nate.com>
2018-08-17 22:34:42 +0000

    before navbar

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,6 +8,7 @@ class Board < ActiveRecord::Base
     accepts_nested_attributes_for :hashtags
 
     belongs_to :profile
+    belongs_to :user
     has_many :likes
     has_many :liked_users, through: :likes, source: :user
     has_many :comments
