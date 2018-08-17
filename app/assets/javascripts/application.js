95b3ff1c993e58907f447da15ca41ff09def9366
Max ShinWoo Chung <donaskwhy@nate.com>
2018-08-17 02:43:35 +0000

    modal demo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,10 @@
 //    $('.tabs').tabs();
 //  });
   
-  
+    $(document).ready(function(){
+    $('.modal').modal();
+  });
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
