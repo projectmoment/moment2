d472fc11c995ff5d7a3b705931173a33fd79c60f
KyungMin Kim <qaws2051@naver.com>
2018-08-19 15:19:37 +0000

    user tag

--- app/models/board.rb
+++ app/models/board.rb
@@ -16,40 +16,5 @@ class Board < ActiveRecord::Base
     # 사람 태그하기. ex) @철수
     has_many :tags
     
-    after_create do
-    usertags = self.content.scan(/@\w+/)
-    usertags.uniq.map do |usertag|
-      if User.find_by(name: usertag.delete('@')).present?
-        user = User.find_by(name: usertag.delete('@'))
-        tag = Tag.find_by(name: usertag.delete('@'),
-                          user_id: user.id,
-                          comment_id: self.id)
-        if tag.nil?
-          Tag.create(name: usertag.delete('@'),
-                     user_id: user.id,
-                     comment_id: self.id)
-        else
-          tag.update(name: usertag.delete('@'),
-                     user_id: user.id,
-                     comment_id: self.id)
-        end
-      end
-    end
-  end
-  
-    # # 해시태그를 고쳐보자! 
-    # after_create do
-    #   hashtags = self.content.scan(/#\w+/) # '#'로 시작하는 문자열 스캔
-    #   hashtags.uniq.map do |hashtag|       
-    #     if Category.find_by(name: hashtag.delete('#')).present?
-    #       category = Category.find_by(name: hashtag.delete('#'))
-    #       tag = Hashtag.find_by(title: hashtag.delete('#') )
-    #       if tag.nil?
-    #         Hashtag.create(title: hashtag.delete('#') )
-    #       else
-    #         tag.update(title: hashtag.delete('#') )
-    #       end
-    #     end
-    #   end
-    # end
 end
+ 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,26 @@
 class Comment < ActiveRecord::Base
   belongs_to :board
+  has_many :tags
+  
+  after_create do
+    usertags = self.body.scan(/@\w+/)
+    usertags.uniq.map do |usertag|
+      if User.find_by(name: usertag.delete('@')).present?
+        user = User.find_by(name: usertag.delete('@'))
+        tag = Tag.find_by(title: usertag.delete('@'), # 태그한 유저 이름
+                          user_id: user.id,           # 위 이름에 해당하는 user id
+                          comment_id: self.id)        # 태그를 포함하는 comment id
+        if tag.nil?  # 유저는 있는데, 생성된 태그가 없다
+          Tag.create(title: usertag.delete('@'),
+                     user_id: user.id,
+                     comment_id: self.id)
+        else         # 유저도 있고, 해당 유저 태그도 있다
+          tag.update(title: usertag.delete('@'),
+                     user_id: user.id,
+                     comment_id: self.id)
+        end
+      end
+    end
+  end
+  
 end
