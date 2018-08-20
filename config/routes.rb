80b26c02e9d915572a3d804f6e7a3f13eeba80ed
Max ShinWoo Chung <donaskwhy@nate.com>
2018-08-20 06:26:39 +0000

    멘탈흔들

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'posts/explore' => 'posts#explore' 
   get 'posts/intro'  => 'posts#intro'
   get 'posts/search' => 'posts#search'
-  resources :posts
+  resources :posts, defaults: { format: 'js' }
   
   get 'profile/:user_id' => 'profile#index', as: 'profile_index'
   get 'profile/mypage/:user_id' => 'profile#mypage', as: 'mypage'
